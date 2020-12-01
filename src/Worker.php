47480e9d7903cc44491a37eca6127f67e55eef78
gaobinzhan <gaobinzhan@gmail.com>
2020-12-01 17:06:04 +0800

    fix: jump out while

--- src/Worker.php
+++ src/Worker.php
@@ -43,31 +43,33 @@ class Worker extends AbstractUnixProcess
          */
         if($this->taskConfig->getTaskQueue()){
             Coroutine::create(function (){
-                while ($this->infoTable->incr($this->workerIndex,'running',1) <= $this->taskConfig->getMaxRunningNum()){
-                    try{
+                while (1){
+                    try {
+                        if ($this->infoTable->get($this->workerIndex, 'running') >= $this->taskConfig->getMaxRunningNum()) {
+                            Coroutine::sleep(0.1);
+                            continue;
+                        }
+
                         $task = $this->taskConfig->getTaskQueue()->pop();
                         if($task){
                             $taskId = $this->taskIdAtomic->add(1);
                             Coroutine::create(function ()use($taskId,$task){
                                 try{
+                                    $this->infoTable->incr($this->workerIndex, 'running', 1);
                                     $this->runTask($task,$taskId);
                                 }catch (\Throwable $throwable){
                                     $this->onException($throwable);
                                 } finally {
                                     $this->infoTable->decr($this->workerIndex,'running',1);
                                 }
-
                             });
                         }else{
-                            $this->infoTable->decr($this->workerIndex,'running',1);
                             Coroutine::sleep(0.1);
                         }
                     }catch (\Throwable $throwable){
-                        $this->infoTable->decr($this->workerIndex,'running',1);
                         $this->onException($throwable);
                     }
                 }
-                Coroutine::sleep(0.1);
             });
         }
         parent::run($arg);
